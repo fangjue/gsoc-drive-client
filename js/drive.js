40d19e1da6736ea7a56dac6f91e71f9b0c98cd55
方觉(Fang Jue) <fangjue23303@gmail.com>
2013-07-05 15:40:42 +0800

    Move sendRequest out of GoogleDrive
    sendRequest is used to send a generic XHR. It's now moved to
    the new RequestSender class and token related code is moved into
    GoogleDrive's sendDriveRequest_ method.

--- js/drive.js
+++ js/drive.js
@@ -5,6 +5,178 @@
 
 'use strict';
 
+var RequestSender = function() {
+  return this;
+};
+
+/**
+ * @typedef {object} MultipartBodyPart
+ * @property {string} content The content of this part as a string.
+ * @property {string} contentType MIME type for this part.
+ * @property {string} encoding Value of 'Content-Transfer-Encoding'.
+ */
+
+/**
+ * @typedef {object} MultipartBodyDetails
+ * @property {string} boundary The boundary string to use.
+ * @property {Array.MultipartBodyPart} parts An array of all parts.
+ */
+
+/**
+ * @typedef {object} HTTPRange
+ * @property {long|string} start Start offset of the range. Can be '*' if
+ *     unknown.
+ * @property {long|string} end End offset of the range, including this byte. Can
+ *     be '*' if unknown.
+ * @property {long|string} total Total number of bytes. Can be '*' if unknown.
+ */
+
+/*
+ * @typedef {object} XHRDetails
+ * @property {object} queryParameters A dictionary containing query parameters.
+ *     Keys are parameter names and values are parameter values. You mustn't
+ *     supply query parameters if the request URL already contains a query
+ *     string.
+ * @property {string} authorization HTTP Authorization header.
+ * @property {HTTPRange} contentRange HTTP Content-Range header.
+ * @property {string} contentType HTTP Content-Type header.
+ * @property {object} headers A dictionary containing additional HTTP headers.
+ *     Keys are HTTP header names and values are HTTP header values.
+ * @property {HTTPRange} range HTTP Range header. Only |start| and |end|
+ *     properties can be used here.
+ * @property {string|Blob|File} body Request body that will be directly passed
+ *     to XMLHttpRequest's send method.
+ * @property {MultipartBodyDetails} multipartBody Multipart request body
+ *     content.
+ * @property {Array.integer} expectedStatus Expected status codes returned
+ *     from the server to indicate success. |callback| is called with xhrError
+ *     property in the error parameter if the actual status code doesn't match
+ *     any of these codes specified.
+ * @property {string} responseType XHR response type, such as 'blob'.
+ */
+
+/**
+ * Send a generic XHR request.
+ * @param {string} method Standard HTTP method.
+ * @param {string} url Request URL.
+ * @param {XHRDetails} details Request details, including request body,
+ *     content type, etc.
+ * @param {function} callback Called with the response.
+ */
+RequestSender.prototype.sendRequest = function(method, url, details,
+    callback) {
+  if (!details)
+    details = {};
+
+  console.assert(!(details.body && details.multipartBody));
+  console.assert(!(details.contentType && details.multipartBody));
+  console.assert(!(url.indexOf('?') != -1 &&
+                   Object.keys(details.queryParametersi || {})).length > 0);
+
+  if (details.queryParameters && url.indexOf('?') == -1)
+    url += this.generateQuery_(details.queryParameters);
+
+  var xhr = new XMLHttpRequest();
+  // TODO: onprogress, upload.onprogress
+
+  xhr.open(method, url, true);
+  if (details.responseType)
+    xhr.responseType = details.responseType;
+  if (details.authorization)
+    xhr.setRequestHeader('Authorization', details.authorization);
+  if (details.contentType)
+    xhr.setRequestHeader('Content-Type', details.contentType);
+
+  if (details.range)
+    xhr.setRequestHeader('Range', this.getRangeHeader_(details.range));
+
+  if (details.contentRange) {
+    xhr.setRequestHeader('Content-Range',
+        this.getContentRangeHeader_(details.contentRange));
+  }
+
+  var requestBody = details.body;
+  if (details.multipartBody) {
+    xhr.setRequestHeader('Content-Type', 'multipart/mixed; boundary="' +
+        details.multipartBody.boundary + '"');
+    requestBody = this.generateMultipartRequestBody_(
+        details.multipartBody);
+  }
+
+  if (details.headers) {
+    Object.keys(details.headers).forEach(function(name) {
+      xhr.setRequestHeader(name, details.headers[name]);
+    });
+  }
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      var error = false;
+      if (details.expectedStatus) {
+        if (details.expectedStatus.indexOf(xhr.status) == -1)
+          error = true;
+      } else if (xhr.status < 200 || xhr.status >= 300)
+        error = true;
+
+      if (error)
+        callback(xhr, {xhrError: {
+            status: xhr.status,
+            response: xhr.response}});
+      else
+        callback(xhr);
+      xhr = null;
+    }
+  };
+
+  xhr.send(requestBody);
+};
+
+RequestSender.prototype.getRangeHeader_ = function(range) {
+  console.assert(!(range.start == null && range.end == null));
+  var start = '';
+  var end = '';
+  if (range.start != null)
+    start = range.start.toString();
+  if (range.end != null)
+    end = range.end.toString();
+  return 'bytes=' + start + '-' + end;
+};
+
+RequestSender.prototype.getContentRangeHeader_ = function(contentRange) {
+  var range = '*';
+  if (contentRange.start != null && contentRange.end != null)
+    range = contentRange.start.toString() + '-' + contentRange.end.toString();
+  return 'bytes ' + range + '/' + (contentRange.total || '*');
+};
+
+RequestSender.prototype.generateQuery_ = function(params) {
+  if (Object.keys(params).length == 0)
+    return '';
+  return '?' + Object.keys(params).map(function(key) {
+    return escape(key) + '=' + escape(params[key]);
+  }).join('&');
+};
+
+/**
+ * Generate the multipart request body.
+ * @param {MultipartBodyDetails} details Multipart body details.
+ * @return {string} Returns the request body string that can be sent directly.
+ */
+RequestSender.prototype.generateMultipartRequestBody_ = function(details) {
+  var crlf = '\r\n';
+  var delimiter = crlf + '--' + details.boundary + crlf;
+  var ending = crlf + '--' + details.boundary + '--';
+  var body = '';
+  return details.parts.map(function(part) {
+    var bodyPart = delimiter;
+    if (part.contentType)
+      bodyPart += 'Content-Type: ' + part.contentType + crlf;
+    if (part.encoding)
+      bodyPart += 'Content-Transfer-Encoding: ' + part.encoding + crlf;
+    return bodyPart + crlf + part.content;
+  }).join('') + ending;
+};
+
 var GoogleDrive = function(opt_options) {
   if (!opt_options)
     opt_options = {};
@@ -35,6 +207,8 @@ var GoogleDrive = function(opt_options) {
         {}, callback);
   };
 
+  this.requestSender_ = new RequestSender();
+
   if (!opt_options.prettyPrint)
     this.prettyPrint_ = 'false';
   this.fields_ = opt_options.fields || {};
@@ -54,159 +228,22 @@ GoogleDrive.prototype.getFilesFields_ = function() {
  * @property {object} xhrError
  */
 
-/**
- * @typedef {object} MultipartBodyPart
- * @property {string} content The content of this part as a string.
- * @property {string} contentType MIME type for this part.
- * @property {string} encoding Value of 'Content-Transfer-Encoding'.
- */
-
-/**
- * @typedef {object} MultipartBodyDetails
- * @property {string} boundary The boundary string to use.
- * @property {Array.MultipartBodyPart} parts An array of all parts.
- */
-
-/*
- * @typedef {object} XHRDetails
- * @property {string} responseType XHR response type, such as 'blob'.
- * @property {object} queryParameters A dictionary containing query parameters.
- *     Keys are parameter names and values are parameter values. This will be
- *     ignored if the URL already contains a query string.
- * @property {string} contentType HTTP Content-Type header.
- * @property {MultipartBodyDetails} multipartBody Multipart request body
- *     content.
- * @property {Array.integer} expectedStatus Expected status codes returned
- *     from the server to indicate success. |callback| is called with xhrError
- *     property in the error parameter if the actual status code doesn't match
- *     any of these codes specified.
- */
-
-/**
- * Send a generic XHR request.
- * @param {string} method 'GET', 'POST' or other HTTP methods.
- * @param {string} url Request URL.
- * @param {XHRDetails} opt_details Request details, including request body,
- *     content type, etc.
- * @param {function} callback Called with the response.
- */
-GoogleDrive.prototype.sendRequest = function(method, url, opt_details,
-    callback) {
-  if (!opt_details)
-    opt_details = {};
-  console.assert(!(opt_details.body && opt_details.multipartBody));
-  console.assert(!(opt_details.contentType && opt_details.multipartBody));
-
+GoogleDrive.prototype.sendDriveRequest_ = function(method, url, options, callback) {
   this.getToken_(function(token) {
     if (!token) {
       callback(null, {tokenError: {details: chrome.runtime.lastError}});
       return;
     }
 
-    if (opt_details.queryParameters && url.indexOf('?') == -1)
-      url += this.generateQuery_(opt_details.queryParameters);
+    options.authorization = 'Bearer ' + token;
 
-    var xhr = new XMLHttpRequest();
-    // TODO: onprogress, upload.onprogress
-
-    xhr.open(method, url, true);
-    if (opt_details.responseType)
-      xhr.responseType = opt_details.responseType;
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-    if (opt_details.contentType)
-      xhr.setRequestHeader('Content-Type', opt_details.contentType);
-
-    if (opt_details.range) {
-      var start = '';
-      var end = '';
-      if (opt_details.range.start != null)
-        start = opt_details.range.start.toString();
-      if (opt_details.range.end != null)
-        end = opt_details.range.end.toString();
-      xhr.setRequestHeader('Range', 'bytes=' + start + '-' + end);
-    }
-
-    if (opt_details.contentRange) {
-      var range = '*';
-      if (opt_details.contentRange.start != null &&
-          opt_details.contentRange.end != null)
-        range = opt_details.contentRange.start.toString() + '-' +
-            opt_details.contentRange.end.toString();
-      xhr.setRequestHeader('Content-Range', 'bytes ' + range + '/' +
-          (opt_details.contentRange.total || '*'));
-    }
-
-    var requestBody = opt_details.body;
-    if (opt_details.multipartBody) {
-      xhr.setRequestHeader('Content-Type', 'multipart/mixed; boundary="' +
-          opt_details.multipartBody.boundary + '"');
-      requestBody = this.generateMultipartRequestBody_(
-          opt_details.multipartBody);
-    }
-
-    if (opt_details.headers)
-      Object.keys(opt_details.headers).forEach(function(name) {
-        xhr.setRequestHeader(name, opt_details.headers[name]);
-      });
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-        var error = false;
-        if (opt_details.expectedStatus) {
-          if (opt_details.expectedStatus.indexOf(xhr.status) == -1)
-            error = true;
-        } else if (xhr.status < 200 || xhr.status >= 300)
-          error = true;
-
-        if (error)
-          callback(xhr, {xhrError: {
-              status: xhr.status,
-              response: xhr.response}});
-        else
-          callback(xhr);
-        xhr = null;
-      }
-    };
-
-    xhr.send(requestBody);
+    if (this.prettyPrint_)
+      if (!options.queryParameters)
+        options.queryParameters = {};
+      if (!options.queryParameters.prettyPrint)
+        options.queryParameters.prettyPrint = this.prettyPrint_;
+    this.requestSender_.sendRequest(method, url, options, callback);
   }.bind(this));
-};
-
-GoogleDrive.prototype.generateQuery_ = function(params) {
-  if (Object.keys(params).length == 0)
-    return '';
-  return '?' + Object.keys(params).map(function(key) {
-    return escape(key) + '=' + escape(params[key]);
-  }).join('&');
-};
-
-/**
- * Generate the multipart request body.
- * @param {MultipartBodyDetails} details Multipart body details.
- * @return {string} Returns the request body string that can be sent directly.
- */
-GoogleDrive.prototype.generateMultipartRequestBody_ = function(details) {
-  var crlf = '\r\n';
-  var delimiter = crlf + '--' + details.boundary + crlf;
-  var ending = crlf + '--' + details.boundary + '--';
-  var body = '';
-  return details.parts.map(function(part) {
-    var bodyPart = delimiter;
-    if (part.contentType)
-      bodyPart += 'Content-Type: ' + part.contentType + crlf;
-    if (part.encoding)
-      bodyPart += 'Content-Transfer-Encoding: ' + part.encoding + crlf;
-    return bodyPart + crlf + part.content;
-  }).join('') + ending;
-};
-
-GoogleDrive.prototype.sendDriveRequest_ = function(method, url, options, callback) {
-  if (this.prettyPrint_)
-    if (!options.queryParameters)
-      options.queryParameters = {};
-    if (!options.queryParameters.prettyPrint)
-      options.queryParameters.prettyPrint = this.prettyPrint_;
-  this.sendRequest(method, url, options, callback);
 };
 
 /**
@@ -607,8 +644,8 @@ GoogleDrive.prototype.untrash = function(fileId, callback) {
  * @param {callback} Called when the request is completed.
  */
 GoogleDrive.prototype.delete = function(fileId, callback) {
-  this.sendRequest('DELETE', this.DRIVE_API_FILES_BASE_URL + '/' + fileId,
-      {}, function(xhr, error) {
+  this.sendDriveRequest_('DELETE',
+      this.DRIVE_API_FILES_BASE_URL + '/' + fileId, {}, function(xhr, error) {
     callback(error);
   });
 };
@@ -760,7 +797,7 @@ GoogleDriveEntry.prototype.download = function(options, callback) {
   if (!options)
     options = {};
 
-  this.drive_.sendRequest('GET', this.details.downloadUrl,
+  this.drive_.requestSender_.sendRequest('GET', this.details.downloadUrl,
       {responseType: 'blob', expectedStatus: [200, 206]}, function(xhr, error) {
         if (error)
           callback(null, error);
